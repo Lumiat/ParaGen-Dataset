3f65a2890a7907cea4f36932f5211b180dc461b3
Lumiat <grace20177102@icloud.com>
2025-08-22 14:32:18 +0000

    modify configuration of BoolQ

---
+++ collect_scripts/common-sense-reasoning/BoolQ/pretrain_run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+llamafactory-cli train qwen2.5-0.5b_boolq_pretrain.yaml
+
+llamafactory-cli train llama-7b_boolq_pretrain.yaml
+
+llamafactory-cli train mistral-7b_boolq_pretrain.yaml
+
+llamafactory-cli train gpt2_boolq_pretrain.yaml
+
+llamafactory-cli train gemma-3-4b_boolq_pretrain.yaml
+
+llamafactory-cli train bert_boolq_pretrain.yaml
