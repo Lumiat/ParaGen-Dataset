0604cc8c688ed08d856cce0acb146a8d049219c3
Lumiat <grace20177102@icloud,com>
2025-08-27 01:14:41 +0800

    add new configuration files: HellaSwag

---
+++ collect_scripts/common-sense-reasoning/HellaSwag/pretrain_run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+llamafactory-cli train qwen2.5-0.5b_hellaswag_pretrain.yaml
+
+llamafactory-cli train llama-7b_hellaswag_pretrain.yaml
+
+llamafactory-cli train mistral-7b_hellaswag_pretrain.yaml
+
+llamafactory-cli train gpt2_hellaswag_pretrain.yaml
+
+llamafactory-cli train gemma-3-4b_hellaswag_pretrain.yaml
+
+llamafactory-cli train bert_hellaswag_pretrain.yaml
